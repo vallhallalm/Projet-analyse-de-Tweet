0c3cf38fd009a7a80ea9726b60685abdd07428e6
SquixDev <Squix@users.noreply.github.com>
2022-05-12 15:24:04 +0200

    Create etape1_prepartion.js

---
+++ etape1_prepartion.js
@@ -0,0 +1,19 @@
+db.small_tweets_collection.aggregate([
+    
+    
+    { $group: 
+            { _id: '$user.id',
+                verified:{$first:'$user.verified'},
+                followers:{$first:'$user.followers_count'},
+                user_created_at:{$first:'$user.created_at'},
+                tweets:{$push:{
+                    text:"$text",
+                    created_at:"$created_at",
+                    urls:'$entities.urls',
+                    hashtags:'$entities.hashtags.text',
+                    mentions_count:{$size:"$entities.user_mentions"}
+                    
+                }},
+                tweets_count:{$first:'$user.statuses_count'},
+                groups_count:{$first:'$user.listed_count'}
+            }}, {$out: "small_tweets_grouped_by_user"}], {allowDiskUse:true})
