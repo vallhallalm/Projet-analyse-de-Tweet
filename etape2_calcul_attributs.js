f64d5fc51c438be6a976bea4bd87ea6223334842
SquixDev <Squix@users.noreply.github.com>
2022-05-19 16:42:32 +0200

    Update etape2_calcul_attributs.js

--- etape2_calcul_attributs.js
+++ etape2_calcul_attributs.js
@@ -4,6 +4,9 @@ db.small_tweets_grouped_interesting_data.aggregate([
     {$addFields: {
          "follow_popularity": {
              $multiply:[{$divide:[ {$subtract: ["$followers",0]}, {$subtract: [30755372,0]}]},100]
+         },
+        "group_popularity": {
+             $multiply:[{$divide:[ {$subtract: ["$groups_count",0]}, {$subtract: [114667,0]}]},100]
          }
     }}
    
